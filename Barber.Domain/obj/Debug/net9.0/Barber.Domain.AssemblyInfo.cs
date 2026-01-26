df1af9ac199a8c247d7811092b36a875a66782bb
Mustafo6252 <ravshanovmustafo88@gmail.com>
2026-01-26 11:18:22 +0500

    the best

--- Barber.Domain/obj/Debug/net9.0/Barber.Domain.AssemblyInfo.cs
+++ Barber.Domain/obj/Debug/net9.0/Barber.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barber.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63515fcb022337472b6182223935294b95f0aee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141942ccc251725cb83e1c72056f86fee0160ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barber.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barber.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
