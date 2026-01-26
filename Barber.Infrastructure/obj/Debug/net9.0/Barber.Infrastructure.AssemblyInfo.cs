141942ccc251725cb83e1c72056f86fee0160ca0
Mustafo6252 <ravshanovmustafo88@gmail.com>
2026-01-26 11:09:42 +0500

    first commit

--- Barber.Infrastructure/obj/Debug/net9.0/Barber.Infrastructure.AssemblyInfo.cs
+++ Barber.Infrastructure/obj/Debug/net9.0/Barber.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barber.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63515fcb022337472b6182223935294b95f0aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barber.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barber.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
